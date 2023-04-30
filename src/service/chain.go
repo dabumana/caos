0761e317b92a1406e73350b428a073fd1c2c8480
dabumana <dabumana@gmail.com>
2023-04-30 01:42:42 -0500

    Update chain.go

--- src/service/chain.go
+++ src/service/chain.go
@@ -1,9 +1,41 @@
 // Package service section
 package service
 
-// Chain - Event sequence object
-type Chain struct {
-	transforms []Transformer
+// ChainedTransforms - Sequence index
+var chainedTransform []Chain
+
+// SequenceHandler - Chained Transform template
+type SequenceHandler []ChainTransforms
+
+// SequencesInterface - Chain sequence interface
+type ChainTransforms interface {
+	Get() []Chain
+	Set(chainedTransform []Chain)
+	// Stack operation
+	Add(chain *Chain)
+	Remove(chain *Chain)
+}
+
+// Get - Retrieve actual chain list
+func (c *SequenceHandler) Get() []Chain {
+	return chainedTransform
+}
+
+// Set - Modify chain list
+func Set(chain []Chain) {
+	chainedTransform = chain
+}
+
+// Add - Add chain to actual stack
+func (c *SequenceHandler) Add(chain *Chain) {
+	chainedTransform = append(chainedTransform, *chain)
+}
+
+// Remove - Remove chain from actual stack
+func (c *SequenceHandler) Remove(chain *Chain) {
+	lastIndex := len(chainedTransform) - 1
+	chain = &(chainedTransform)[lastIndex]
+	chainedTransform = (chainedTransform)[:lastIndex]
 }
 
 // onConstructAssemble - Assemble transformer
@@ -14,6 +46,11 @@ func (c *Transformer) onConstructImplementation() {}
 
 // onConstructValidation - Valdiate transformer
 func (c *Transformer) onConstructValidation() {}
+
+// Chain - Event sequence object
+type Chain struct {
+	transforms []Transformer
+}
 
 // PrepareChain - Initialize chain sequence
 func (c *Chain) PrepareChain() {
